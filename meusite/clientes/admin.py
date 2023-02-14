50d24c667e3f535e5a8004ea543c49c07eb492c7
Fábio Ferreira <f_fabio@ymail.com>
2023-02-14 15:23:49 -0300

    Atualização

--- meusite/clientes/admin.py
+++ meusite/clientes/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Cliente, Telefone
+from .models import Cliente, Telefone, CPF, Departamento
 
 admin.site.register(Cliente)
 admin.site.register(Telefone)
+admin.site.register(CPF)
+admin.site.register(Departamento)
--- meusite/clientes/models.py
+++ meusite/clientes/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.nome
+
+class CPF(models.Model):
+    numero = models.CharField(max_length=15)
+    data_exp = models.DateTimeField(auto_now=False)
+       
+# Create your models here.   
 class Cliente(models.Model):
     nome = models.CharField(max_length=70, null=False)
     endereco = models.CharField(max_length=200, blank=False, null=False)
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    CPF = models.OneToOneField(CPF, null=True, blank=True, on_delete=models.CASCADE)
+    departamentos = models.ManyToManyField(Departamento, null=True, blank=True)    
     
     def __str__(self):
         """String for representing the Model object."""
@@ -20,3 +32,4 @@ class Telefone(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.descricao + ' - ' + self.numero
+    
