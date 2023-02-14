c93ec16deb1db4de4ce4a6869c6caf9f394ce44b
Fábio Ferreira <f_fabio@ymail.com>
2023-02-14 15:59:02 -0300

    Aula meus_templates

--- meusite/meusite/settings.py
+++ meusite/meusite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meusite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['meus_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
