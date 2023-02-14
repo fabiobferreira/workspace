0799117c19709e0bf855f30cce9dd072967e8de4
Fábio Ferreira <f_fabio@ymail.com>
2023-02-14 17:25:04 -0300

    Atualização Condicionais e Loops

--- meusite/meusite/settings.py
+++ meusite/meusite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meusite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['meus_templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- meusite/meusite/views.py
+++ meusite/meusite/views.py
@@ -2,4 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    sexo = 'm'
+    nome = 'Pedro'
+    lista = [
+        {'nome': 'Pedro', 'sexo': 'm'},
+        {'nome': 'Maria', 'sexo': 'f'},
+        {'nome': 'Joaquina', 'sexo': 'f'},
+        {'nome': 'João', 'sexo': 'm'},
+    ]
+    dados = {'lista': lista, 'sexo': sexo, 'nome': nome}
+    return render(request, 'index.html', dados)
