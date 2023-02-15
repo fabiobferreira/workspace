b0e4292ffc46bd6bc03beb3c89e208f56f61a454
Fábio Ferreira <f_fabio@ymail.com>
2023-02-15 13:47:53 -0300

    Aula criando App Core

--- ProjetoFinal/core/models.py
+++ ProjetoFinal/core/models.py
@@ -7,12 +7,22 @@ class Pessoa(models.Model):
     endereco = models.CharField(max_length=200)
     telefone = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.nome
+
 class Marca(models.Model):
-    nome = models.CharField(max_length=50)    
+    nome = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.nome    
     
 class Veiculo(models.Model):
+    proprietario = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     placa = models.CharField(max_length=7)
     cor = models.CharField(max_length=15)
-    observacoes = models.TextFiel()
+    observacoes = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.marca + ' - ' + self.cor + ' - ' + self.placa
     
