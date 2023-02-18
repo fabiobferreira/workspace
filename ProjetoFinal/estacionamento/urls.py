e4b486ec901fdf28bb8fa92216eb4bbe75ae83b2
Fábio Ferreira <f_fabio@ymail.com>
2023-02-18 10:04:00 -0300

    GitIgnore

--- ProjetoFinal/estacionamento/urls.py
+++ ProjetoFinal/estacionamento/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
+from django.urls import path
 from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
