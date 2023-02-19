36430bcb8975081d1be2be5d9c373c095188ac7c
Fábio Ferreira <f_fabio@ymail.com>
2023-02-19 18:38:24 -0300

    Aula Salvando Contatos no Banco de Dados

--- ProjetoFinal/website/views.py
+++ ProjetoFinal/website/views.py
@@ -6,6 +6,15 @@ def home(request):
     return render(request, 'website/index.html')
 
 def contato(request):
+    contato = {}
+    contato['nome'] = request.POST.get('nome')
+    contato['email'] = request.POST.get('email') 
+    contato['endereco'] = request.POST.get('endereco') 
+    contato['cidade'] = request.POST.get('cidade') 
+    contato['uf'] = request.POST.get('uf')
+    contato['cep'] = request.POST.get('cep') 
+    contato['mensagem'] = request.POST.get('mensagem')
+    contato['receber_noticias'] = request.POST.get('receber_noticias')     
     return render(request, 'website/contato.html')
 
 def servicos(request):
